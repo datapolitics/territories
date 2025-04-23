1bb55a1a867f103930dab24d6dae2bf4ee36abf5
timothee <timothee@datapolitics.fr>
2025-04-23 10:26:41 +0200

    feat: changed build tool to include data

--- tests/test_data.py
+++ tests/test_data.py
@@ -9,3 +9,8 @@ def test_legacy_codes():
         Territory.load_tree_from_bytes(gzip.decompress(file.read()))
 
     assert Territory.from_tu_ids("f81d4fae-7dec-11d0-a765-00a0c91e6bf6") == Territory.from_tu_ids("DEP:69", "DEP:75")
+
+
+            # if "EPCI" in name and not name in epci_to_comm:
+            #     name = name.split('-')[0]
+            # return epci_to_comm.get(name, name)
