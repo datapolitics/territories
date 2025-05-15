090d2a9094c2d1e84544a76243268d9e7fc4bf46
timothee <timothee@datapolitics.fr>
2025-05-15 14:58:58 +0200

    fix: better public API

--- src/territories/__init__.py
+++ src/territories/__init__.py
@@ -1,3 +1,14 @@
-from territories.territories import Territory
-from territories.partitions import TerritorialUnit, Partition
-from territories.exceptions import MissingTreeException, MissingTreeCache, NotOnTreeError
+# this tells both users and type checkers exactly which names are part of the public API
+__all__ = [
+    "Territory",
+    "TerritorialUnit",
+    "Partition",
+    "MissingTreeException",
+    "MissingTreeCache",
+    "NotOnTreeError",
+]
+
+
+from .territories import Territory
+from .partitions import TerritorialUnit, Partition
+from .exceptions import MissingTreeException, MissingTreeCache, NotOnTreeError
