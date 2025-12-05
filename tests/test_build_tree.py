4f2531008bc2f8ff37b336cf81c8d7841a8c0e1f
timothee <timothee@datapolitics.fr>
2025-12-05 11:51:33 +0100

    feat: ruff tests

--- tests/test_build_tree.py
+++ tests/test_build_tree.py
@@ -1,29 +1,21 @@
-import gzip
-import pytest
-
 from territories import Territory
-from territories.database import stream_tu_table, create_connection, async_stream_tu_table, async_create_connection, NodeTuple
+from territories.database import NodeTuple
 
 
 def test_build_tree():
-
     nodes = [
-        NodeTuple(id='CNTRY:France', label='France', level='CNTRY', parent_id=None),
-        NodeTuple(id='REG:Sud', label='Sud', level='REG', parent_id='CNTRY:France'),
-        NodeTuple(id='REG:idf', label='île-de-france', level='REG', parent_id='CNTRY:France'),
-
-        NodeTuple(id='DEP:Rhone', label='Rhône', level='DEP', parent_id='REG:Sud'),
-        NodeTuple(id='DEP:metropole', label='Grand Lyon', level='DEP', parent_id='REG:Sud'),
-
-        NodeTuple(id='COM:Pantin', label='Pantin', level='COM', parent_id="REG:idf"),
-        NodeTuple(id='COM:Nogent', label='Nogent', level='COM', parent_id="REG:idf"),
-        NodeTuple(id='COM:Paris', label='Paris', level='COM', parent_id="REG:idf"),
-
-        NodeTuple(id='COM:sté', label='Saint Étienne', level='COM', parent_id="DEP:Rhone"),
-        NodeTuple(id='COM:Lyon', label='Lyon', level='COM', parent_id="DEP:metropole"),
-        NodeTuple(id='COM:Villeurbane', label='Villeurbane', level='COM', parent_id="DEP:metropole"),
-
-        NodeTuple(id='COM:Marseille', label='Marseille', level='COM', parent_id="REG:Sud"),
+        NodeTuple(id="CNTRY:France", label="France", level="CNTRY", parent_id=None),
+        NodeTuple(id="REG:Sud", label="Sud", level="REG", parent_id="CNTRY:France"),
+        NodeTuple(id="REG:idf", label="île-de-france", level="REG", parent_id="CNTRY:France"),
+        NodeTuple(id="DEP:Rhone", label="Rhône", level="DEP", parent_id="REG:Sud"),
+        NodeTuple(id="DEP:metropole", label="Grand Lyon", level="DEP", parent_id="REG:Sud"),
+        NodeTuple(id="COM:Pantin", label="Pantin", level="COM", parent_id="REG:idf"),
+        NodeTuple(id="COM:Nogent", label="Nogent", level="COM", parent_id="REG:idf"),
+        NodeTuple(id="COM:Paris", label="Paris", level="COM", parent_id="REG:idf"),
+        NodeTuple(id="COM:sté", label="Saint Étienne", level="COM", parent_id="DEP:Rhone"),
+        NodeTuple(id="COM:Lyon", label="Lyon", level="COM", parent_id="DEP:metropole"),
+        NodeTuple(id="COM:Villeurbane", label="Villeurbane", level="COM", parent_id="DEP:metropole"),
+        NodeTuple(id="COM:Marseille", label="Marseille", level="COM", parent_id="REG:Sud"),
     ]
 
     Territory.build_tree(nodes, save_tree=False)
